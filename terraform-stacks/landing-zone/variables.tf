2beb33a967aa18aeef98c6b948e2e1b18a5aee9e
Ludovico Caldara <ludovico.caldara@oracle.com>
2026-03-02 18:04:45 +0100

    fix lz variables

--- terraform-stacks/landing-zone/variables.tf
+++ terraform-stacks/landing-zone/variables.tf
@@ -6,7 +6,10 @@ variable "region" { type = string }
 
 variable "compartment_ocid" { type = string }
 
-variable "name_prefix" { type = string default = "lab-lz" }
+variable "name_prefix" {
+   type = string
+   default = "lab-lz"
+}
 
 variable "vcn_cidr" { type = string default = "10.50.0.0/16" }
 
@@ -15,13 +18,27 @@ variable "bastion_subnet_cidr" {
   default = "10.50.1.0/24"
 }
 
-variable "enable_nat_gateway" { type = bool default = true }
-variable "enable_service_gateway" { type = bool default = true }
+variable "enable_nat_gateway" {
+  type = bool
+  default = true
+}
+
+variable "enable_service_gateway" {
+  type = bool
+  default = true
+}
 
 variable "client_cidr_block_allow_list" {
   type        = list(string)
   description = "Public egress CIDRs allowed to initiate Bastion sessions (e.g., corporate VPN/NAT)."
 }
 
-variable "defined_tags"  { type = map(string) default = {} }
-variable "freeform_tags" { type = map(string) default = {} }
+variable "defined_tags"  {
+  type = map(string)
+  default = {}
+}
+
+variable "freeform_tags" {
+  type = map(string)
+  default = {}
+}
