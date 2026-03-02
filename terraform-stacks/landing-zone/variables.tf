810fa5dbd776399e0a8a6d0bffe6784f7e6e9c1e
Ludovico Caldara <ludovico.caldara@oracle.com>
2026-03-02 18:25:54 +0100

    fix lz variables

--- terraform-stacks/landing-zone/variables.tf
+++ terraform-stacks/landing-zone/variables.tf
@@ -1,7 +1,4 @@
 variable "tenancy_ocid" { type = string }
-variable "user_ocid" { type = string }
-variable "fingerprint" { type = string }
-variable "private_key_path" { type = string }
 variable "region" { type = string }
 
 variable "compartment_ocid" { type = string }
@@ -43,5 +40,5 @@ variable "defined_tags"  {
 
 variable "freeform_tags" {
   type = map(string)
-  default = {}
+  default = {stack = "landing-zone"}
 }
