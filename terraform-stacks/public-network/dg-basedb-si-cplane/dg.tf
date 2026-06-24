32a05fd0d91f5b815420d7317e84aee16189e252
Ludovico Caldara <ludovico.caldara@oracle.com>
2026-06-24 16:30:43 +0200

    fix: wrong provider version

--- terraform-stacks/public-network/dg-basedb-si-cplane/dg.tf
+++ terraform-stacks/public-network/dg-basedb-si-cplane/dg.tf
@@ -12,7 +12,8 @@ resource "oci_database_db_system" "standby_db_system" {
   data_storage_percentage = var.data_storage_percentage
   data_storage_size_in_gb = var.data_storage_size_in_gb
   database_edition        = var.db_edition
-  source_db_system_id    = oci_database_db_system.db_system.id
+  source_db_system_id     = oci_database_db_system.db_system.id
+  primary_db_system_id    = oci_database_db_system.db_system.id
   source                  = "DATAGUARD"
 
   db_home {
@@ -22,6 +23,9 @@ resource "oci_database_db_system" "standby_db_system" {
     database {
       admin_password               = var.db_admin_password
       db_unique_name = "${var.lab_name}_site2"
+      protection_mode    = "MAXIMUM_PERFORMANCE"
+      transport_type     = "ASYNC"
+      is_active_data_guard_enabled = true
     }
   }
 
--- terraform-stacks/public-network/dg-basedb-si-cplane/providers.tf
+++ terraform-stacks/public-network/dg-basedb-si-cplane/providers.tf
@@ -1,12 +1,6 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = "~> 6.30"
-    }
-  }
 }
 
 provider "oci" {
